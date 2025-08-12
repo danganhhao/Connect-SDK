1607f4b0de5d298f6d6fb8627a69cd33f3517811
Đặng Anh Hào <danganhhao8a3vts@gmail.com>
2025-08-12 17:29:54 +0700

    Update version + remove *.so file

--- core/src/com/connectsdk/service/webos/lgcast/common/connection/ConnectionManagerError.java
+++
@@ -1,8 +0,0 @@
-package com.connectsdk.service.webos.lgcast.common.connection;
-
-public enum ConnectionManagerError {
-    GENERIC_ERROR,
-    CONNECTION_CLOSED,
-    DEVICE_SHUTDOWN,
-    RENDERER_TERMINATED
-}
--- core/src/com/connectsdk/service/webos/lgcast/common/connection/LGCastCommandListener.java
+++
@@ -1,7 +0,0 @@
-package com.connectsdk.service.webos.lgcast.common.connection;
-
-import org.json.JSONObject;
-
-public interface LGCastCommandListener {
-    void onReceive(JSONObject response);
-}
